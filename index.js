0f809ecda60adb690ddf405bd2e24e16a5e561f1
nickcodesbetter <nicholas.kenyon.may@gmail.com>
2021-07-13 17:48:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const age = prompt ("How old are you?")
+undefined
+age
+"26"
+const distance = prompt ("How many km are you travelling?")
+undefined
+distance
+"26"
+// if (age < 16) { 
+// VM2040:1 Uncaught SyntaxError: Unexpected end of input
+// newtab/:1 Unchecked runtime.lastError: The message port closed before a response was received.
+// if (age < 16) prompt((distance * 0.21) / 5);
+// undefined
+// else if (age > 65 prompt((distance * 0.21) / 5 
+// VM2936:1 Uncaught SyntaxError: Unexpected token 'else'
+// else prompt(distance * 0.21)
+// VM3075:1 Uncaught SyntaxError: Unexpected token 'else'
+const price = (distance * 0.21)
+undefined
+price
+5.46
+const under16Discount = (price / 5)
+undefined
+under16Discount
+1.092
+under16DiscountApplied = (price - under16Discount)
+4.368
+const under16DiscountApplied = (price - under16Discount)
+undefined
+under16DiscountApplied
+4.368
+if (age < 16) confirm("under16DiscountApplied")
+over65Discount = (price / 2.5)
+2.184
